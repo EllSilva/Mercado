b36a38139b5691b91f77762fbc70dca9ed3b32b7
unknown <joaquimmigueldecristo@gmail.com>
2024-05-22 10:45:02 -0400

    first commit3

--- shop/assets/js/components/menu/home.js
+++ shop/assets/js/components/menu/home.js
@@ -3,9 +3,14 @@ import get_template from '../../components/get_template.js'
 export default {
     data: function () {
         return {
+
+            isActive1: false,
+            isActive2: false,
+            isActive3: false,
+
             title: "home",
-            activo: false, 
-            activoService: false, 
+            activo: false,
+            activoService: false,
             activoSite: false,
             activoDesign: false,
             activoMarketig: false,
@@ -19,149 +24,94 @@ export default {
     },
 
     methods: {
+        mostrar() {
+            this.isActive2 = !this.isActive2;
+            this.isActive3 = !this.isActive3;
+        },
+
+
+
+
+
+
+        mostrarMenu() {
+            this.activo = !this.activo;
+        },
  
-        mostrarMenu() {  
-            this.activo = !this.activo; 
-        },
-      
-        mostrarMenuService() {  
-            this.activoService = !this.activoService;  
+        mostrarSite() {
+            this.activoSite = !this.activoSite;
         },
 
-          
-        mostrarSite() {  
-            this.activoSite = !this.activoSite;  
-        },  
-
-        mostrarDesign() {  
-            this.activoDesign = !this.activoDesign;  
-        },
-        
-        mostrarMarketig() {  
-            this.activoMarketig = !this.activoMarketig;  
-        },
-        
-        mostrarOutros() {  
-            this.activoOutros = !this.activoOutros;  
-        },
-        
-        fechaTodosMenu() {  
-            this.activo = !this.activo; 
-            this.activoService = !this.activoService;  
+        mostrarDesign() {
+            this.activoDesign = !this.activoDesign;
         },
 
-        homeMenu() { 
-                this.activo5 = false,
+        mostrarMarketig() {
+            this.activoMarketig = !this.activoMarketig;
+        },
+
+        mostrarOutros() {
+            this.activoOutros = !this.activoOutros;
+        },
+
+        fechaTodosMenu() {
+            this.activo = !this.activo;
+            this.activoService = !this.activoService;
+        },
+
+        homeMenu() {
+            this.activo5 = false,
                 this.activo4 = false,
                 this.activo3 = false,
                 this.activo2 = false,
                 this.activo1 = true,
-                this.activo = !this.activo; 
+                this.activo = !this.activo;
         },
 
         sobreMenu() {
             this.activo5 = false,
-            this.activo4 = false,
-            this.activo3 = false,
-            this.activo2 = true,
-            this.activo1 = false
-            this.activo = !this.activo; 
+                this.activo4 = false,
+                this.activo3 = false,
+                this.activo2 = true,
+                this.activo1 = false
+            this.activo = !this.activo;
         },
 
         servicoMenu() {
             this.activo5 = false,
-            this.activo4 = false,
-            this.activo3 = true,
-            this.activo2 = false,
-            this.activo1 = false,
-            this.activo = !this.activo; 
+                this.activo4 = false,
+                this.activo3 = true,
+                this.activo2 = false,
+                this.activo1 = false,
+                this.activo = !this.activo;
         },
 
         galeriaMenu() {
             this.activo5 = false,
-            this.activo4 = true,
-            this.activo3 = false,
-            this.activo2 = false,
-            this.activo1 = false,
-            this.activo = !this.activo; 
+                this.activo4 = true,
+                this.activo3 = false,
+                this.activo2 = false,
+                this.activo1 = false,
+                this.activo = !this.activo;
         },
 
         contactoMenu() {
             this.activo5 = true,
-            this.activo4 = false,
-            this.activo3 = false,
-            this.activo2 = false,
-            this.activo1 = false,
-            this.activo = !this.activo; 
+                this.activo4 = false,
+                this.activo3 = false,
+                this.activo2 = false,
+                this.activo1 = false,
+                this.activo = !this.activo;
         },
 
-         
-      
+
+
     },
 
     async mounted() {
-      
 
- const menu = document.querySelector(".menu");
- const menuMain = menu.querySelector(".menu-main");
- const goBack = menu.querySelector(".go-back");
- const menuTrigger = document.querySelector(".mobile-menu-trigger");
- const closeMenu = menu.querySelector(".mobile-menu-close");
- let subMenu;
- menuMain.addEventListener("click", (e) =>{
- 	if(!menu.classList.contains("active")){
- 		return;
- 	}
-   if(e.target.closest(".menu-item-has-children")){
-   	 const hasChildren = e.target.closest(".menu-item-has-children");
-      showSubMenu(hasChildren);
-   }
- });
- goBack.addEventListener("click",() =>{
- 	 hideSubMenu();
- })
- menuTrigger.addEventListener("click",() =>{
- 	 toggleMenu();
- })
- closeMenu.addEventListener("click",() =>{
- 	 toggleMenu();
- })
- document.querySelector(".menu-overlay").addEventListener("click",() =>{
- 	toggleMenu();
- })
- function toggleMenu(){
- 	menu.classList.toggle("active");
- 	document.querySelector(".menu-overlay").classList.toggle("active");
- }
- function showSubMenu(hasChildren){
-    subMenu = hasChildren.querySelector(".sub-menu");
-    subMenu.classList.add("active");
-    subMenu.style.animation = "slideLeft 0.5s ease forwards";
-    const menuTitle = hasChildren.querySelector("i").parentNode.childNodes[0].textContent;
-    menu.querySelector(".current-menu-title").innerHTML=menuTitle;
-    menu.querySelector(".mobile-menu-head").classList.add("active");
- }
-
- function  hideSubMenu(){  
-    subMenu.style.animation = "slideRight 0.5s ease forwards";
-    setTimeout(() =>{
-       subMenu.classList.remove("active");	
-    },300); 
-    menu.querySelector(".current-menu-title").innerHTML="";
-    menu.querySelector(".mobile-menu-head").classList.remove("active");
- }
  
- window.onresize = function(){
- 	if(this.innerWidth >991){
- 		if(menu.classList.contains("active")){
- 			toggleMenu();
- 		}
 
- 	}
- }
-
-
-
-      },
+    },
     template: await get_template('./assets/js/components/menu/home')
 }
